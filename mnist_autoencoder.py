e770d920001de5de0aff9aea32112c5d5070434e
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-01-16 21:18:43 -0800

    e

--- mnist_autoencoder.py
+++ mnist_autoencoder.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-
+import tqdm
+print("imported")
 # Set random seed for reproducibility
 torch.manual_seed(42)
 learning_rate = 0.001
@@ -17,6 +18,7 @@ mnist_dataset = datasets.MNIST(
     root='./data', train=True, transform=transform, download=True)
 data_loader = DataLoader(dataset=mnist_dataset,
                          batch_size=batch_size, shuffle=True)
+print("data loaded")
 
 
 class Autoencoder_v1(nn.Module):
@@ -49,13 +51,13 @@ class Autoencoder_v1(nn.Module):
 
 
 # Instantiate model, define loss function, and optimizer
-model = Autoencoder()
+model = Autoencoder_v1()
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
+print("model initialized")
 # Training loop
-for epoch in range(num_epochs):
-    for data, _ in data_loader:
+for epoch in tqdm.trange(num_epochs):
+    for data, _ in tqdm.tqdm(data_loader):
         inputs = data.view(data.size(0), -1)
 
         optimizer.zero_grad()
@@ -65,6 +67,7 @@ for epoch in range(num_epochs):
         optimizer.step()
 
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+    torch.save(model.state_dict(), 'mnist_autoencoder.pth')
 
 # Save the trained model if needed
 torch.save(model.state_dict(), 'mnist_autoencoder.pth')
