c163c5fdabcadd429dbba2e56ba2d83b59febb2c
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-01-18 14:09:16 -0800

    oops fixed

--- inpainting_model.py
+++ inpainting_model.py
@@ -10,8 +10,19 @@ def black_out_random_rectangle(tensor):
         rect_height = torch.randint(1, int(height/4), (1,)).item()
         rect_width = torch.randint(1, int(width/4), (1,)).item()
         # Black out the selected rectangle in all channels for the current image
-        tensor[i, :, top:max(top+rect_height, height), left:max(left+rect_width,width)] = 0
+        tensor[i, :, top:min(top+rect_height, height), left:min(left+rect_width,width)] = 0
 
+def black_out_random_rectangle_legacy(tensor):
+    batch_size, num_channels, height, width = list(tensor.shape)
+
+    for i in range(batch_size):
+        # Randomly select the position and size of the rectangle
+        top = torch.randint(0, height-2, (1,)).item()
+        left = torch.randint(0, width-2, (1,)).item()
+        rect_height = torch.randint(1, height-top, (1,)).item()
+        rect_width = torch.randint(1, width-left, (1,)).item()
+        # Black out the selected rectangle in all channels for the current image
+        tensor[i, :, top:min(top+rect_height, height), left:min(left+rect_width,width)] = 0
 class Autoencoder_CAE(nn.Module):
     def __init__(self):
         super(Autoencoder_CAE, self).__init__()
