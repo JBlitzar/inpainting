a8b31906a6b8fef298b8c14f75cfea2b31da7a5b
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-01-18 16:20:38 -0800

    stuff.

--- inpainting_model.py
+++ inpainting_model.py
@@ -75,11 +75,11 @@ class Autoencoder_CAEv2(nn.Module):
         self.decoder = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Upsample(scale_factor=2, mode='bilinear'),
             
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Upsample(scale_factor=2, mode='bilinear'),
             
             nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid()
