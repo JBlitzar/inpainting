b5fbf4c343f5b7149bfa61936189a1946e1118e4
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-01-20 13:09:14 -0800

    v3 bad. Making a new v3

--- inpainting_model.py
+++ inpainting_model.py
@@ -97,17 +97,14 @@ class Autoencoder_CAEv3(nn.Module):
 
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            #nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
             nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
-            #nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
             nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
-            #nn.BatchNorm2d(16),
             nn.ReLU()
         )
 
@@ -121,7 +118,7 @@ class Autoencoder_CAEv3(nn.Module):
             nn.ReLU(),
             nn.Upsample(scale_factor=2, mode='nearest'),
             
-            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid()
         )
 
